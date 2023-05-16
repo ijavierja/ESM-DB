780d10d1bb14c2ec996ae648ae18281bb1e4f6bf
Javier Lim <javierlimja96@gmail.com>
2023-05-16 17:49:25 +0800

    create employees table

---
+++ migrations/20230516091319_create_employees.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable("employees", (table) => {
+    table.string("id");
+    table.string("login");
+    table.string("name");
+    table.decimal("salary");
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable("employees")
+};
