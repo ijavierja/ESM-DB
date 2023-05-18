93dfdde46182025e39350ccbe55b955c628940b0
Javier Lim <javierlimja96@gmail.com>
2023-05-18 10:35:09 +0800

    alter salary column to numeric

---
+++ migrations/20230518021748_alter_employees.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable("employees", (table) => {
+    table.decimal("salary", 12, 2).alter();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable("employees", (table) => {
+    table.string("salary").alter();
+  });
+};
